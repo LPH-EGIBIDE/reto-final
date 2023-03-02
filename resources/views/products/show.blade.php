a34563d0a75c5642db031819f3bba39d3d77fc96
Yeray <yeray.bote@ikasle.egibide.org>
2023-03-02 12:25:11 +0100

    Show product

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('navbar')
+    @include('navbar')
+@endsection
+
+@section('content')
+<div class="container mt-5 mb-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="">
+                            <div class="p-4">
+                                <img src="https://images3.alphacoders.com/597/597575.jpg" width="480" height="300" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="product p-4">
+                            <form action="" method="POST">
+                            @csrf
+                                <div class="d-flex justify-content-between align-items-center">
+                                <div class="d-flex align-items-center"> <i class="fa fa-long-arrow-left p-1"></i> <a href="/" class="ml-1 text-muted">Volver</a> </div> <i class="fa fa-shopping-cart text-muted"></i>
+                            </div>
+                            <div class="mt-4 mb-1"> <span class="text-uppercase text-muted brand">Segundo</span>
+                                <h5 class="text-uppercase">Brocheta de pescado y verduritas</h5>
+                                <div class="price d-flex flex-row align-items-center"> <span class="act-price p-1">$25</span>
+                                    <div class="ml-2"> 
+                                        <small class="dis-price">$50</small> <span>50% DESCUENTO</span> 
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="about">Shop from a wide range of t-shirt from orianz. Pefect for your everyday use, you could pair it with a stylish pair of jeans or trousers complete the look.</p>
+                            <div class="form-outline">
+                                <input type="number" id="typeNumber" class="form-control" value="1" min="1"/>
+                                <label class="form-label" for="typeNumber">Cantidad</label>
+                            </div>
+                            <div class="cart mt-4 align-items-center">
+                                <input type="submit" class="btn btn-danger text-uppercase mr-2 px-4" value="Añadir al carro">
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('footer')
+    @include('footer')
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/producto', function () {
+    return view('products.show');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
